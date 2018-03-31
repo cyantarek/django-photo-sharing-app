6ef28f462d34f162da8a084dcb393b53b68f71a9
Cyan Tarek <cyantarek.cg@gmail.com>
2018-03-31 13:35:41 +0600

    Heroku deplpoyment configured

--- chhuti_job/settings.py
+++ chhuti_job/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#sf33f9)$$1x=#g$j2z)l9!1owmilvg@+)zv43e@&693*a6f^g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com','127.0.0.1']
 
